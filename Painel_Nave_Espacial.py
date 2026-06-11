ad5062328552a9d9620d32b3cad5b8dca5b1bcbf
anacsilva60-cpu <ana.c.silva60@edu.senai.br>
2026-06-11 09:55:53 -0300

    Adicionada a função de remover tripulantes da Nave

--- Painel_Nave_Espacial.py
+++ Painel_Nave_Espacial.py
@@ -21,14 +21,26 @@ def status_nave():
     print(f"Temos {combustível} de Combustível")
     print(f"Os Tripulantes são {tripulantes}")
     print("-" * 32 + "\n")
+
 def registrar_tripulante():
     novo_tripulante = input("Qual o nome do Tripulante?: ")
     tripulantes.append(novo_tripulante)
     print("Tripulante Inserido com Sucesso!!🧑‍🚀🪪")
 
+def suprimir_tripulante():
+    if len(tripulantes) == 0:
+        print("Não há Tripulantes para remover!!")
+        print("Adicione um Tripulante para prosseguir...")
+    else:
+        ## tripulante_removido = input("Qual o nome do Tripulante que quer retirar?: ") 
+        ## tripulantes.remove(tripulante_removido)
+        tripulantes.pop()
+        print("Tripulante Removido com Sucesso!!🧑‍🚀🪪")
+        print(f"Os Tripulantes presentes na Nave são: {tripulantes}")
+
 print("\n----> Bem Vindo ao Menu Interativo da Nave!🎛️ 🎚️  Por Favor, selecione uma opção: ")
 while True: ## ----> Loop roda de sempre!!
-    print("\n1- Mostrar Status da Nave | 2- Viajar | 3- Abastecer | 4- Novo Tripulante | 5- Sair")
+    print("\n1- Mostrar Status da Nave | 2- Viajar | 3- Abastecer | 4- Novo Tripulante | 5- Remover Tripulante | 6- Sair ")
     opção = input("Escolha: ")
     if(opção == "1"):
         status_nave()
@@ -39,22 +51,7 @@ while True: ## ----> Loop roda de sempre!!
     elif(opção == "4"):
         registrar_tripulante()
     elif(opção == "5"):
+        suprimir_tripulante()
+    elif(opção == "¨6"):
         print("Viagem Encerrada!")
         break 
-
-
-# status_nave()
-# registrar_tripulante()
-# registrar_tripulante()
-# registrar_tripulante()
-# status_nave()
-
-# status_nave()
-# viajar()
-# viajar()
-# status_nave()
-# viajar()
-# viajar()
-# abastecer()
-# viajar()
-# status_nave()
